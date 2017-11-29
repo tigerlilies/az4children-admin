710f84b6101ca51ffc35b1f3bd197a45ecba7baa
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2017-11-29 10:54:22 -0700

    update ChildForm

--- src/components/ChildForm.js
+++ src/components/ChildForm.js
@@ -20,11 +20,17 @@ class ChildForm extends Component {
       firstname: values.fname,
       age: values.age,
       gender: values.gender,
-      phone: values.phone,
-      email: values.email,
-      source: values.source,
-      assignedto: values.assignedto,
-      comments: values.comments
+      summary: values.summary,
+      characteristic1: values.characteristic1,
+      characteristic2: values.characteristic2,
+      characteristic3: values.characteristic3,
+      need1: values.need1,
+      need2: values.need2,
+      need3: values.need3,
+      placement: values.placement,
+      placement_phone: values.placementphone,
+      placement_email: values.placementemail,
+      zone: values.zone
     };
 
     if (this.props.match.params.id === 'add') {
@@ -138,10 +144,10 @@ class ChildForm extends Component {
               <label htmlFor="zone" className="col-lg-2 control-label">Zone:</label>
               <div className="col-lg-10">
                 <div className="btn-group" role="group" aria-label="Basic example">
-                <button type="button"  name="north" value="north" className="btn btn-secondary">North</button>
-                <button type="button"  name="east" value="east" className="btn btn-secondary">East</button>
-                <button type="button"  name="south" value="south" className="btn btn-secondary">South</button>
-                <button type="button"  name="west" value="west" className="btn btn-secondary">West</button>
+                <button type="button"  name="zone" value="north" className="btn btn-secondary">North</button>
+                <button type="button"  name="zone" value="east" className="btn btn-secondary">East</button>
+                <button type="button"  name="zone" value="south" className="btn btn-secondary">South</button>
+                <button type="button"  name="zone" value="west" className="btn btn-secondary">West</button>
                 </div>
               </div>
             </div>
