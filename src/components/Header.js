1daa5efeed6b77682716b49d9dcdf29f73291aac
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2018-01-13 11:14:14 -0700

    v2 first update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+
 
 class Header extends Component {
+
+  renderLinks() {
+    return (
+      <li className="nav-item">
+        <a className="nav-link" href="/login">Login</a>
+      </li>
+    )
+  }
+  
   render() {
     return (
       <ul className="nav justify-content-end">
         <li className="nav-item">
           <a className="nav-link active" href="/">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/login">Login</a>
-        </li>
+        {this.renderLinks()}
       </ul>
     )
   }
